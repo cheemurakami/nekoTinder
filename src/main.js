8b05e6ceed8f0b5d47aa73e0a83b1e674e594e78
Gabe D <gpad88@gmail.com>
2020-04-26 16:00:00 -0700

    Adds a matcher class do randomly get a match or not

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 import $ from "jquery";
+import { Matcher } from "./Matcher";
 
-$(document).ready(function(){
-
+$(document).ready(function () {
+  let matcher = new Matcher();
+  if (matcher.isMutual()) {
+    console.log("Likes you back!!");
+  } else {
+    console.log("Aww not mutual");
+  }
 });
